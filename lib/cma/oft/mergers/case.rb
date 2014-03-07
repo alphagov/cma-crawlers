bd2acfb5bc39c5cebddc3aa281c65b7d62454eda
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-03-07 14:01:18 +0000

    Piling more into OFT::Mergers::Case before the second class shows up
    * Allow merger case to save itself

--- lib/cma/oft/mergers/case.rb
+++ lib/cma/oft/mergers/case.rb
@@ -1,4 +1,6 @@
 require 'nokogiri'
+require 'cma/case_store'
+require 'uri'
 
 module CMA
   module OFT
@@ -8,8 +10,39 @@ module CMA
       class Case
         attr_accessor :title, :sector, :original_url
 
+        def base_name
+          @base_name ||= URI.parse(original_url).path[1..-1].gsub('/', '-')
+        end
+
         def assets
           []
+        end
+
+        def save!
+          CMA::CaseStore.instance.save(self)
+        end
+
+        def self.load(filename)
+          CMA::CaseStore.instance.load(filename)
+        end
+
+        def case_type
+          'mergers'
+        end
+
+        ##
+        # Relative to the CaseStore
+        def filename
+          File.join(case_type, base_name) + '.json'
+        end
+
+        def to_json
+          {
+            'title'        => title,
+            'case_type'    => case_type,
+            'sector'       => sector,
+            'original_url' => original_url,
+          }
         end
 
         def self.from_case_list_row(row)
--- spec/cma/oft/case_spec.rb
+++ spec/cma/oft/case_spec.rb
@@ -1,8 +1,23 @@
+require 'spec_helper'
 require 'cma/oft/mergers/case'
 
 module CMA
   module OFT
     describe Mergers::Case do
+      let(:doc)    { Nokogiri::HTML(File.read('spec/fixtures/oft/mergers.html')) }
+      let(:row)    { doc.at_xpath('//table/tr[1]') }
+
+      shared_examples 'it has all the row properties of Alliance Medical' do
+        it                 { should be_a(Mergers::Case) }
+        its(:title)        { should eql('Alliance Medical / IBA Molecular') }
+        its(:case_type)    { should eql('mergers') }
+        its(:sector)       { should eql('Healthcare') }
+        its(:original_url) { should eql('http://oft.gov.uk/OFTwork/mergers/Mergers_Cases/2013/Alliance') }
+        its(:base_name)    { should eql('OFTwork-mergers-Mergers_Cases-2013-Alliance')}
+        its(:filename)     { should eql('mergers/OFTwork-mergers-Mergers_Cases-2013-Alliance.json')}
+        its(:assets)       { should be_empty }
+      end
+
       describe '.from_case_list_row' do
         context 'when row is nil' do
           it 'raises an ArgumentError' do
@@ -12,16 +27,28 @@ module CMA
         end
 
         context 'the happy path' do
-          let(:doc)    { Nokogiri::HTML(File.read('spec/fixtures/oft/mergers.html')) }
-          let(:row)    { doc.at_xpath('//table/tr[1]') }
-
           subject      { Mergers::Case.from_case_list_row(row) }
 
-          it                 { should be_a(Mergers::Case) }
-          its(:title)        { should eql('Alliance Medical / IBA Molecular') }
-          its(:sector)       { should eql('Healthcare') }
-          its(:original_url) { should eql('http://oft.gov.uk/OFTwork/mergers/Mergers_Cases/2013/Alliance') }
-          its(:assets)       { should be_empty }
+          it_should_behave_like 'it has all the row properties of Alliance Medical'
+        end
+      end
+
+      describe 'serializing to the document store' do
+        let(:mergers_case)  { Mergers::Case.from_case_list_row(row) }
+        let(:expected_path) { 'spec/fixtures/store/mergers/OFTwork-mergers-Mergers_Cases-2013-Alliance.json' }
+
+        before do
+          mergers_case.save!
+        end
+
+        it 'saves to the default output path' do
+          expect(File).to exist(expected_path)
+        end
+
+        describe 'loading it back' do
+          subject { Mergers::Case.load(expected_path) }
+
+          it_should_behave_like 'it has all the row properties of Alliance Medical'
         end
       end
     end
