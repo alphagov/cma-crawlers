68c3d57dd343dece312b77343decc79b7355f907
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-03-10 16:20:39 +0000

    Fix for zero-length assets

--- lib/cma/oft/mergers/crawler.rb
+++ lib/cma/oft/mergers/crawler.rb
@@ -47,7 +47,7 @@ module CMA
           when page.url.to_s =~ ASSET
             _case = find_nearest_case_matching(page.referer, CASE)
             if _case
-              asset = CMA::Asset.new(page.url, _case, page.doc, page.headers['content-type'])
+              asset = CMA::Asset.new(page.url, _case, page.body, page.headers['content-type'])
               asset.save!
               _case.assets << asset
               _case.save!
