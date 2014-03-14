8bcda51f17819a359a726bd7fe7006f162162785
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-03-14 18:24:48 +0000

    Some sanity-checking regex matches for CC crawler

---
+++ spec/cma/cc/crawler_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'cma/cc/our_work/crawler'
+
+module CMA::CC::OurWork
+  describe Crawler do
+    describe 'what should match what' do
+      specify do
+        'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/tradebe-sita'.
+          should match(Crawler::CASE)
+      end
+      specify do
+        'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/tradebe-sita/news-releases'.
+        should match(Crawler::SUBPAGE)
+      end
+      specify do
+        'http://www.competition-commission.org.uk/assets/anything.pdf'.
+        should match(Crawler::ASSET)
+      end
+    end
+  end
+end
